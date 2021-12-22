7462194691ca0d237c0f8904417b5da3fdf2a21a
cstemmer <96531901+cstemmer@users.noreply.github.com>
2021-12-23 00:44:38 +0400

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Conrad')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Conrad');
           done();
         });
     });
@@ -36,10 +36,13 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({"surname":"Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200,'response status should be 200');
+          assert.equal(res.type, 'application/json', 'response should be json');
+          assert.equal(res.body.name, 'Cristoforo', 'body name should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 'body surname should be Colombo');
           done();
         });
     });
